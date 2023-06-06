23f9b43311114a664305eca39ef2d650900e04de
Benedict Arhin <benedict.arhin@amalitech.com>
2023-06-06 12:40:24 +0000

    Started Customer Reviews component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import LandingPage from './pages/LandingPage'
 
 function App() {
 
   return (
     <>
       <div>Hello</div>
+      <LandingPage/>
     </>
   )
 }
 
-export default App
+export default App;
