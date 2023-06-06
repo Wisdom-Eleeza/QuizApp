ed4303e24e2647d4cdd45a89897be43e265af238
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-06-06 15:41:45 +0000

    Feature: Working on create account api

--- backend/models/registerModel.js
+++ backend/models/registerModel.js
@@ -1,26 +1,37 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is required'],
-        minlength: 5,
-        maxlength: 255,
-    },
-    email: {
-        type: String,
-        required: [true, 'Email field is required'],
-        minlength: 5,
-        maxlength: 255,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true, 'Password is a required field'],
-        minlength: 5,
-        maxlength: 1024,
-    }
-})
+const registerUserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: 5,
+    maxlength: 255,
+  },
+  email: {
+    type: String,
+    required: [true, "Email field is required"],
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is a required field"],
+    minlength: 5,
+    maxlength: 1024,
+  },
+});
 
+const registerModel = mongoose.model("User", registerUserSchema);
 
+function validateRegisterUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(255).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(1024).required(),
+  });
+  return schema.validate(user);
+}
+
+module.exports = { registerModel, validateRegisterUser };
