1de1b02f7bfff8095c9b03803bb0a4efb0fb92dc
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-06-07 20:45:23 +0000

    Feature: Install jsonwebtoken and generating a token

--- backend/controller/registerController.js
+++ backend/controller/registerController.js
@@ -4,7 +4,7 @@ const {
 } = require("../models/registerModel");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const upload = require('../middleware/uploadMiddleware')
+const upload = require("../middleware/uploadMiddleware");
 const connectDB = require("../config/db");
 const express = require("express");
 const router = express.Router();
@@ -18,26 +18,27 @@ const registerUser = async (req, res) => {
   const { error } = validateRegisterUser(req.body); // validateUser is to validate only user registration
   if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await registerModel.findOne({ email: req.body.email });
-  if (user) {
-    // if user already in the database, send BadRequest(400)
-    return res.status(400).send("User already registered");
-  } else {
-    // if not in the database, create new user
-    user = new registerModel({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      profileImage: imagePath,
-    });
-  }
-
-  //hashed the user password
   try {
+    let user = await registerModel.findOne({ email: req.body.email });
+    if (user) {
+      // if user already in the database, send BadRequest(400)
+      return res.status(400).send("User already registered");
+    } else {
+      // if not in the database, create new user
+      user = new registerModel({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        profileImage: imagePath,
+      });
+    }
+
+    //hashed the user password
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
-    res.send(user);
+
+    //Generate a JWT token for the user
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
