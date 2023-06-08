761ccc8cded8fb9d67cadadc712fd3469f01f3ad
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-06-08 14:19:42 +0000

    Feature: Added middleware route

--- backend/controller/imageController.js
+++ backend/controller/imageController.js
@@ -12,19 +12,11 @@ const uploadImage = async (req, res) => {
     console.log(req.file);
     if(!uploadedFile) return res.status(400).send("Image is required")
     const newImage = new imageModel({name: uploadedFile.originalname, filePath: uploadedFile.path });
+    await newImage.save();
     res.status(200).json({ message: "Image uploaded successfully", filePath: uploadedFile.path});
   } catch (error) {
-    
+    res.status(500).json({ error: "Failed to upload image" });
   }
-
-  
-
-  // try {
-  //   await newImage.save();
-  //   res.status(200).json({ message: "Image uploaded successfully" });
-  // } catch (error) {
-  //   res.status(500).json({ error: "Failed to upload image" });
-  // }
 };
 
 module.exports = uploadImage;
