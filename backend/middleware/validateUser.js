08aab2a8632b52d30db3741a7242eecce43b3d1f
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-06-12 16:20:40 +0000

    Fixing unhandle promise rejection

--- backend/middleware/validateUser.js
+++ backend/middleware/validateUser.js
@@ -2,8 +2,8 @@ const Joi = require("joi");
 
 function validateRegisterUser(user) {
   const schema = Joi.object({
-    name: Joi.string().min(5).max(255).required(),
-    email: Joi.string().min(5).max(50).required().email(),
+    name: Joi.string().min(10).max(255).required(),
+    email: Joi.string().max(50).required().email(),
     password: Joi.string().min(10).max(255).required(),
   });
   return schema.validate(user);
