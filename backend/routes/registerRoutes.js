1f9852469d5590b977d897d73fb5b755a41ca91b
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-06-12 16:05:30 +0000

    Fixing unhandle promise rejection

--- backend/routes/registerRoutes.js
+++ backend/routes/registerRoutes.js
@@ -3,6 +3,6 @@ const { registerUser } = require("../controller/registerController");
 const router = express.Router();
 
 router.post("/", registerUser);
-router.post("/profile", registerUser);
+// router.post("/profile", registerUser);
 
 module.exports = router;
