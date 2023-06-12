e3f505027eb7c4c6ded8c5636b52638bbed12185
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-06-12 07:39:03 +0000

    Feature: Landing page controller created

--- backend/routes/imageRoutes.js
+++
@@ -1,9 +0,0 @@
-// const upload = require("../middleware/uploadMiddleware");
-// const express = require("express");
-// const uploadImage = require('../controller/imageController');
-// const router = express.Router();
-// const multer = require("multer");
-
-// router.post("/", uploadImage);
-
-// module.exports = router;
