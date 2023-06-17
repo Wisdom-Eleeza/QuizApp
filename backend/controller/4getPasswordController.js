0f62fcada674e2425d259e0de5381f996650b42d
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-06-17 22:32:17 +0000

    Working on forget password api

--- backend/controller/4getPasswordController.js
+++ backend/controller/4getPasswordController.js
@@ -1,12 +1,12 @@
 const userModel = require("../models/userModels");
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
 
 // @desc Register new user
 // @route POST /api/login
 // @access Public
 const forgetPassword = async () => {
-  const { id, email } = req.body;
+  const { email } = req.body;
   try {
     let user = await userModel.findOne({ email: email });
     if (!user) {
@@ -14,14 +14,39 @@ const forgetPassword = async () => {
         .status(404)
         .json({ success: false, message: "User does not exist" });
     }
-    const token = jwt.sign({ email: user.email, id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: '20m'
-    })
-    const link = `http://localhost:8080/reset-password/${user.id}/${token}`
+    const token = jwt.sign(
+      { email: user.email, id: user._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "20m",
+      }
+    );
+    const link = `http://localhost:8080/reset-password/${user.id}/${token}`;
 
     // nodemailer transporter for sending the reset password email
     let transporter = nodemailer.createTransport({
-        
-    })
-  } catch (error) {}
+      service: "gmail",
+      auth: {
+        user: "your-email@gmail.com",
+        password: "your-password",
+      },
+    });
+    let mailOptions = {
+      from: "your-email@gmail.com",
+      to: "recipient-email@gmail.com",
+      subject: "Password Reset",
+      text: link,
+    };
+
+    // Sending the password reset email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        res.status(400).json({ success: false, message: "Failed to send email" });
+      } else {
+        res.status(200).json({ success: true, message: "Email sent" });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
