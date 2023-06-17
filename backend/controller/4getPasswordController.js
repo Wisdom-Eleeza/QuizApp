aa629168c0bd09896249690a6a24bb4ec6ab29c3
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-06-17 22:08:40 +0000

    Writing the controller logic

--- backend/controller/4getPasswordController.js
+++ backend/controller/4getPasswordController.js
@@ -1,11 +1,12 @@
 const userModel = require("../models/userModels");
+const nodemailer = require('nodemailer')
 const bcrypt = require("bcrypt");
 
 // @desc Register new user
 // @route POST /api/login
 // @access Public
 const forgetPassword = async () => {
-  const { email } = req.body;
+  const { id, email } = req.body;
   try {
     let user = await userModel.findOne({ email: email });
     if (!user) {
@@ -13,6 +14,14 @@ const forgetPassword = async () => {
         .status(404)
         .json({ success: false, message: "User does not exist" });
     }
-    const token = jwt.sign({ email: user.email, id: user._id }, process.env.JWT_SECRET)
+    const token = jwt.sign({ email: user.email, id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '20m'
+    })
+    const link = `http://localhost:8080/reset-password/${user.id}/${token}`
+
+    // nodemailer transporter for sending the reset password email
+    let transporter = nodemailer.createTransport({
+        
+    })
   } catch (error) {}
 };
