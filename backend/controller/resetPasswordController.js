d7b160f989980c76c496588395a2cec3b1881f79
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-06-20 13:22:53 +0000

    Fixed error in the resetPassword Controller

--- backend/controller/resetPasswordController.js
+++ backend/controller/resetPasswordController.js
@@ -1,16 +1,15 @@
 const userModel = require("../models/userModels");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
-// @desc Register new user
+// @desc Reset user password
 // @route POST /api/resetPassword
 // @access Public
 const resetPassword = async (req, res) => {
   try {
     const { id, token } = req.params;
-    const { password } = req.body //Retrieved the password from the request body
-    const user = await userModel.findById({ id });
+    const { password } = req.body; // Retrieved the password from the request body
+    const user = await userModel.findById(id);
 
     if (!user) {
       return res
@@ -20,10 +19,13 @@ const resetPassword = async (req, res) => {
     try {
       jwt.verify(token, process.env.JWT_SECRET);
       // Additional actions after successful token verification
-      // return res.redirect(`/reset-password/${id}/${token}`);
-      user.password = await bcrypt.hash(password, 10) // hashing the new password before updating
-      await user.save(); // saving the updated user
+      const hashedPassword = await bcrypt.hash(password, 10); // Hashing the new password before updating
+      user.password = hashedPassword;
+      await user.save(); // Saving the updated user
       // return res.redirect("http://localhost:8080/api/login");
+      return res
+        .status(200)
+        .json({ success: true, message: "Password reset successful" });
     } catch (error) {
       if (error instanceof jwt.TokenExpiredError) {
         return res
@@ -33,7 +35,7 @@ const resetPassword = async (req, res) => {
       return res.status(400).json({ success: false, message: "Invalid token" });
     }
   } catch (error) {
-    res.status(400).json({ success: false, message: "Not verified" });
+    return res.status(404).json({ success: false, message: "Not verified" });
   }
 };
 
