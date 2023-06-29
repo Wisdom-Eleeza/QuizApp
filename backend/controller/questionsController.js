aa2f4ea68282b4374620c2166edf36fd758d6da0
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-06-29 13:00:12 +0000

    Working on taking quize questions controller

--- backend/controller/questionsController.js
+++ backend/controller/questionsController.js
@@ -1,14 +1,17 @@
 const data = require("../models/dataModel");
 
-// @desc Reset user password
-// @route POST /api/users/questions?page=1&limit=10
+// @desc Fetching Question from database
+// @route GET /api/users/questions?page=1&limit=10
 // @access Private
 const questions = async (req, res) => {
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 10;
+  const page = parseInt(req.query.page) || 1; // Current page number
+  let limit = parseInt(req.query.limit) || 10; // Number of items per page
 
-  // Calculate the maximum limit that can be skipped
-  const skip = (page - 1) * limit;
+  // Validating limit to ensure its not greater than 10
+  limit = Math.min(limit, 10)
+
+  // Calculating the maximum limit that can be skipped
+  const skip = Math.max((page - 1) * limit, 0);
 
   try {
     const fetchedQuestions = await data
